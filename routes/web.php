091f84c795ef64f4ac8e5ef88107620e37ab69b0
Mashal Hussain <2144169@talnet.nl>
2023-10-19 19:00:26 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/planeten', function () {
     return $planeten;
 });
 
-Route::get('/', function () {
+Route::get('/planets', function () {
     $planets = [
         [
             'name' => 'Mars',
@@ -38,5 +38,32 @@ Route::get('/', function () {
         ],
     ];
     return view("welcome", ['planets' => $planets]);
+});
+
+Route::get('/planets/{name}', function ($name) {
+    $planets = [
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
+        ],
+        [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+        ],
+    ];
+    $planet = collect($planets)->firstWhere('name', $name);
+
+    if (!$planet) {
+        return redirect('/planets');
+    }
+    return view("planets", ['planet' => $planet]);
 });
 
